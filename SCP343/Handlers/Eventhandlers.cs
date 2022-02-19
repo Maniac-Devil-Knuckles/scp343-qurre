1f9ea0c3f469bf07de3646187e31d2c18301fa81
Maniac Devil Knuckles <theknuckles.ru@gmail.com>
2022-02-19 19:25:50 +0300

    Update Eventhandlers.cs

--- SCP343/Handlers/Eventhandlers.cs
+++ SCP343/Handlers/Eventhandlers.cs
@@ -35,7 +35,7 @@ namespace SCP343.Handlers
             invisiblePlayers.Clear();
             if (!string.IsNullOrEmpty(Scp343.CustomConfig.Translation.unitname))
             {
-                if (!string.IsNullOrEmpty(Scp343.CustomConfig.Translation.class_d_unit)) if (Scp343.CustomConfig.Translation.class_d_unit.ToLower() != "None") Round.AddUnit(TeamUnitType.ClassD, Scp343.CustomConfig.Translation.class_d_unit);
+                if (!string.IsNullOrEmpty(Scp343.CustomConfig.Translation.class_d_unit)) if (Scp343.CustomConfig.Translation.class_d_unit.ToLower() != "none") Round.AddUnit(TeamUnitType.ClassD, Scp343.CustomConfig.Translation.class_d_unit);
                 Round.AddUnit(TeamUnitType.ClassD, Scp343.CustomConfig.Translation.unitname);
             }
         }
