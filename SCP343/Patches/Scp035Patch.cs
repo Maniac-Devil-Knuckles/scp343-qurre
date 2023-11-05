4c4af4d08285b513d1939cdb92d6fa4f7c862907
Maniac Devil Knuckles <theknuckles.ru@gmail.com>
2023-11-05 16:07:25 +0300

    Scp035 patch

---
+++ SCP343/Patches/Scp035Patch.cs
@@ -0,0 +1,65 @@
+﻿using Qurre.API;
+using Qurre.API.Attributes;
+using Qurre.API.Controllers;
+using Qurre.Events.Structs;
+using SCP343.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCP343.Patches
+{
+    internal static class Scp035
+    {
+
+        internal static Assembly Assembly { get; set; }
+
+        internal static Type Logic
+        {
+            get => Assembly.GetTypes().First(t=> t.Name == "Logic" && t.IsClass);
+        }
+
+        internal static Type plugin
+        {
+            get => Assembly.GetTypes().First(t => t.IsClass && t.Name == "Plugin");
+        }
+
+        internal static bool Prefix(object __instance, PickupItemEvent ev)
+        {
+            if (!ev.Player.IsSCP343()) return true;
+            Eventhandlers.PickupScp035 = true;
+            if (Config.ItemsToConvert.Contains(ev.Pickup.Type) && Config.ItemsConvertToggle)
+            {
+                try
+                {
+                    var fieldinfo = plugin.GetField("Items", BindingFlags.Static | BindingFlags.Public);
+                    var pickup = (List<Pickup>)fieldinfo.GetValue(null);
+                    pickup.Remove(ev.Pickup);
+                    fieldinfo.SetValue(null, pickup);
+                }
+                catch (Exception ex)
+                {
+                    Log.Error(0);
+                    Log.Info(ex);
+                }
+
+                try
+                {
+                    Logic.GetMethod("RefreshItems", BindingFlags.Static | BindingFlags.Public).Invoke(__instance, null);
+                }
+                catch (Exception exception)
+                {
+                    Log.Error(1);
+                    Log.Info(exception);
+                }
+                ev.Pickup.Destroy();
+                foreach(var item in Config.ConvertedItems) ev.Player.Inventory.AddItem(item);
+                ev.Allowed = false;
+            }
+            return false;
+        }
+    }
+}
--- SCP343/Scp343.cs
+++ SCP343/Scp343.cs
@@ -6,10 +6,14 @@ using Qurre.API.Attributes;
 using SCP343.Commands;
 using CommandSystem;
 using RemoteAdmin;
+using System.IO;
+using System.Reflection;
+using System.Linq;
+using SCP343.Patches;
 
 namespace SCP343
 {
-    [PluginInit("SCP-343","Maniac Devil Knuckles","5.0.0")]
+    [PluginInit("SCP-343","Maniac Devil Knuckles","5.0.0.1")]
     public static class Scp343
     {
         public static Harmony harmony { get; internal set; } = null;
@@ -44,29 +48,15 @@ namespace SCP343
                 Spawn343 = new Spawn343();
                 CommandProcessor.RemoteAdminCommandHandler.RegisterCommand(Spawn343);
                 GameCore.Console.singleton.ConsoleCommandHandler.RegisterCommand(Spawn343);
-                /*
                 try
                 {
-                    if (Pathes.Plugins.Contains("scp035"))
-                    {
-                        Log.Info("Patching method scp035...");
-                        PluginInit plugin = PluginManager.plugins.First(p => p.Name == "scp035" && p.Developer == "fydne");
-                        var pickupMethod =
-                            AccessTools.Method(
-                                plugin.GetType().Assembly.GetTypes().First(t => t.IsClass && t.Name == "EventHandlers"),
-                                "PickupItem");
-
-                        var patchprefix = AccessTools.Method(typeof(Scp035), nameof(Scp035.Prefix));
-                        harmony.Patch(pickupMethod, new HarmonyMethod(patchprefix));
-                        Log.Info("Successfully");
-                    }
+                    PatchScp035();
                 }
                 catch (Exception e)
                 {
                     Log.Info("smth not working");
                     Log.Error(e);
                 }
-                */
                 Log.Info("Enabling SCP343 by Maniac Devil Knuckles");
                 
             }
@@ -90,5 +80,41 @@ namespace SCP343
             harmony = null;
         }
 
+        internal static void PatchScp035()
+        {
+            foreach (string pluginString in Directory.GetFiles(Pathes.Plugins))
+            {
+                try
+                {
+                    Assembly pl = Assembly.Load(LoaderManager.ReadFile(pluginString));
+                    if (pl == null) continue;
+                    PluginInit plugin = null;
+                    foreach (var type in pl.GetTypes())
+                    {
+                        PluginInit init = type.GetCustomAttribute<PluginInit>();
+                        if (init == null)
+                            continue;
+                        if (init.Developer == "DaNoNe" && init.Name == "SCP035")
+                        {
+                            plugin = init;
+                            break;
+                        }
+                    }
+                    if (plugin == null) continue;
+                    var pickupMethod =
+                        AccessTools.Method(
+                            pl.GetTypes().First(t => t.IsClass && t.Name == "EventHandler"),
+                            "PickupItemEvent");
+
+                    Log.Info("Patching method scp035...");
+                    Scp035.Assembly = pl;
+                    var patchprefix = AccessTools.Method(typeof(Scp035), nameof(Scp035.Prefix));
+                    harmony.Patch(pickupMethod, new HarmonyMethod(patchprefix));
+                    Log.Info("Successfully");
+                    break;
+                }
+                catch (Exception ex) { Log.Error(ex); }
+            }
+        }
     }
 }
